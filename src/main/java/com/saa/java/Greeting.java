14ab5f5b8eb25452197039cc40e02240f682f9b0
sarellanes <santiagoaray@hotmail.com>
2017-09-04 14:55:36 -0500

    maven project

---
+++ src/main/java/com/saa/java/Greeting.java
@@ -0,0 +1,13 @@
+package com.saa.java;
+
+/**
+ * @author Santiago Arellanes	
+ * @param args
+ */
+public class Greeting {
+
+	public String greet(String name){
+		return "Helloooo "+ name + "###";
+	}
+
+}
---
+++ src/main/java/com/saa/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.saa.java;
+
+public class HelloWorld {
+
+	/**
+	 * @author Santiago Arellanes	
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting obj = new Greeting();
+		System.out.println(obj.greet("Jenkins"));
+	}	
+}
